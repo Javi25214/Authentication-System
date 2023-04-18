c68d37e828caaa5f43b5a342e84e89fcdb233159
Javi25214 <javitoyrosita@gmail.com>
2023-04-18 13:53:47 +0000

    primer commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,15 +4,43 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
+user[]
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+    @api.route('/singup', methods=['POST'])
+    def singup():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    user = User()
+    user.email = username
+    user.password = password
+    user.is_active = True
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user), 200
+    
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+    @api.route('/login', methods=['POST'])
+    def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    user = User.filter.query(username=username, password=password).first()
+    if user is None:
+          # el usuario no se encontró en la base de datos
+    return jsonify({"msg": "Bad username or password"}), 401
+    
+    # crea un nuevo token con el id de usuario dentro
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
+    pass
 
-    return jsonify(response_body), 200
+
+
+    @api.route('/private', methods=['GET'])
+    @jwt_requiered()
+    def private():
+    return "esta es una ruta protegida joder!!", 200
+    
